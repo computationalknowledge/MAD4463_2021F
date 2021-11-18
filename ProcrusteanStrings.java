8178a91e327e09e3852a1b7ce187b2af6c01513b
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2021-11-18 10:08:52 -0500

    Create ProcrusteanStrings.java

---
+++ ProcrusteanStrings.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+/*
+
+Now iterate over your ArrayList and force each string to be 20 characters in Length
+
+Output the result.
+
+ */
+
+public class Peanut {
+
+    public static void main(String[] args) {
+        (new WorkerClass()).Run();
+    }
+
+}
+
+class WorkerClass {
+    public static ArrayList Data;
+    public void Run() {
+        String dataString = "peanut"; 
+        Data = new ArrayList();
+        // Make an ArrayList of 20 randomly generated strings, of random random length
+        for (int i = 1; i <= 20; i++) {
+            Data.add(dataString);
+        }
+
+    }
+
+}
+
+class ProcrutseanAlgorithm {
+
+    public void Run() {
+
+    }
+
+}
